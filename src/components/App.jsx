a67761e22a36286a86b8f14277f0a7d1b3438389
zaklance <wosewick@gmail.com>
2024-05-14 15:15:55 -0400

    done

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import blogData from "../data/blog";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Header from "./Header";
+
+console.log(blogData);
+
+function App() {
+  return (
+    <div className='App'>
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
+    </div>
+  );
+}
+
+export default App;
